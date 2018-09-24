a43ce345001308f212bf351f2250ebb2b0cfeda7
Andrew Halle <ahalle@berkeley.edu>
2018-09-23 20:27:20 -0700

    fix simple add/list, bump version number

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"fmt"
-	"bufio"
 	"strconv"
 	"strings"
 
 	//"github.com/nsf/termbox-go"
-	"github.com/urfave/cli"
 	"github.com/google/uuid"
+	"github.com/urfave/cli"
 
 	"github.com/andrewhalle/todo/task"
 )
@@ -21,7 +21,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "todo"
 	app.Usage = "decide what to do next using your favorite scheduling algorithm"
-	app.Version = "0.0.1"
+	app.Version = "0.0.2"
 	app.Commands = []cli.Command{
 		{
 			Name:   "list",
@@ -127,40 +127,40 @@ func clean(c *cli.Context) error {
 }
 
 func list(c *cli.Context) error {
-		wd, _ := os.Getwd()
-		dir := todoDirectoryPath(wd)
-		tasks := task.FromDir(dir)
-		for _, t := range tasks {
-			fmt.Println("Name: ", t.Name)
-			fmt.Println("Time to Complete: ", t.TimeToComplete)
-			fmt.Println("Priority: ", t.Priority)
-			fmt.Println("")
-		}
-		return nil
-	/*
-	err := termbox.Init()
-	if err != nil {
-		log.Fatal("error: ", err)
+	wd, _ := os.Getwd()
+	dir := todoDirectoryPath(wd)
+	tasks := task.FromDir(dir)
+	for _, t := range tasks {
+		fmt.Println("Name: ", t.Name)
+		fmt.Println("Time to Complete: ", t.TimeToComplete)
+		fmt.Println("Priority: ", t.Priority)
+		fmt.Println("")
 	}
-	defer termbox.Close()
-	w, h := termbox.Size()
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	for i := 0; i < w; i++ {
-		termbox.SetCell(i, 0, '#', termbox.ColorDefault, termbox.ColorDefault)
-	}
-	for i := 1; i < h - 2; i++ {
-		termbox.SetCell(0, i, '#', termbox.ColorDefault, termbox.ColorDefault)
-		termbox.SetCell(w - 1, i, '#', termbox.ColorDefault, termbox.ColorDefault)
-	}
-	for i := 0; i < w; i++ {
-		termbox.SetCell(i, h - 2, '#', termbox.ColorDefault, termbox.ColorDefault)
-	}
-	termbox.SetCell(0, h - 1, ':', termbox.ColorDefault, termbox.ColorDefault)
-	termbox.SetCursor(w / 2, h / 2)
-	fmt.Println("hello world")
-	termbox.Flush()
-	time.Sleep(5 * time.Second)
 	return nil
+	/*
+		err := termbox.Init()
+		if err != nil {
+			log.Fatal("error: ", err)
+		}
+		defer termbox.Close()
+		w, h := termbox.Size()
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		for i := 0; i < w; i++ {
+			termbox.SetCell(i, 0, '#', termbox.ColorDefault, termbox.ColorDefault)
+		}
+		for i := 1; i < h - 2; i++ {
+			termbox.SetCell(0, i, '#', termbox.ColorDefault, termbox.ColorDefault)
+			termbox.SetCell(w - 1, i, '#', termbox.ColorDefault, termbox.ColorDefault)
+		}
+		for i := 0; i < w; i++ {
+			termbox.SetCell(i, h - 2, '#', termbox.ColorDefault, termbox.ColorDefault)
+		}
+		termbox.SetCell(0, h - 1, ':', termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCursor(w / 2, h / 2)
+		fmt.Println("hello world")
+		termbox.Flush()
+		time.Sleep(5 * time.Second)
+		return nil
 	*/
 }
 
@@ -169,15 +169,16 @@ func add(c *cli.Context) error {
 	check(err)
 	dir := todoDirectoryPath(wd)
 	inputReader := bufio.NewReader(os.Stdin)
-	fmt.Println("Name: ")
+	fmt.Print("Name: ")
 	name, err := inputReader.ReadString('\n')
+	name = strings.TrimSpace(name)
 	check(err)
-	fmt.Println("Time remaining: ")
+	fmt.Print("Time remaining: ")
 	ttlStr, err := inputReader.ReadString('\n')
 	check(err)
 	ttl, err := strconv.ParseFloat(strings.TrimSpace(ttlStr), 64)
 	check(err)
-	fmt.Println("Priority: ")
+	fmt.Print("Priority: ")
 	priorityStr, err := inputReader.ReadString('\n')
 	check(err)
 	priority, err := strconv.Atoi(strings.TrimSpace(priorityStr))
